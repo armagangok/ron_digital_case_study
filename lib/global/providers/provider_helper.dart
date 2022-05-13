edd493f6b910705231311cc29fcbb7fade90ed62
Armağan Gök <1armagangok@gmail.com>
2022-05-14 00:21:43 +0300

    ProviderHelper class is being implented

--- lib/global/providers/provider_helper.dart
+++ lib/global/providers/provider_helper.dart
@@ -1,1 +1,21 @@
-/*Dummy file*/
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+import '../../core/view-models/page_indicator_viewmodel.dart';
+import '../../screens/app/home/view-models/controller_provider.dart';
+import '../../screens/app/home/view-models/qr_viewmodel.dart';
+import '../../screens/auth/providers/checkbox_viewmodel.dart';
+import '../../screens/auth/providers/controller_provider.dart';
+
+class ProviderHelper {
+  List<SingleChildWidget> providers = [
+    // ChangeNotifierProvider(create: (_) => GlobalViewModel()),
+    // ChangeNotifierProvider(create: (_) => PageIndicator()),
+    ChangeNotifierProvider(create: (_) => CheckBoxViewModel()),
+    ChangeNotifierProvider(create: (_) => IndicatorViewModel()),
+    ChangeNotifierProvider(create: (_) => QrViewModel()),
+    Provider(create: (_) => SigninPageControllerProvider()),
+    Provider(create: (_) => HomeScreenControllerProvider()),
+    Provider(create: (_) => RegisterPageControllerProvider()),
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:ron_digital/screens/app/home/home.dart';
-import 'package:ron_digital/screens/app/home/view-models/qr_viewmodel.dart';
 
-import './screens/app/home/view-models/controller_provider.dart';
-import './screens/auth/providers/checkbox_viewmodel.dart';
-import './screens/auth/providers/controller_provider.dart';
-import 'core/view-models/page_indicator_viewmodel.dart';
+import './global/providers/provider_helper.dart';
+import './screens/app/home/home.dart';
 
 void main() {
   runApp(
     MultiProvider(
-      providers: [
-        // ChangeNotifierProvider(create: (_) => GlobalViewModel()),
-        // ChangeNotifierProvider(create: (_) => PageIndicator()),
-        ChangeNotifierProvider(create: (_) => CheckBoxViewModel()),
-        ChangeNotifierProvider(create: (_) => IndicatorViewModel()),
-        ChangeNotifierProvider(create: (_) => QrViewModel()),
-        Provider(create: (_) => SigninPageControllerProvider()),
-        Provider(create: (_) => HomeScreenControllerProvider()),
-        Provider(create: (_) => RegisterPageControllerProvider()),
-      ],
+      providers: ProviderHelper().providers,
       child: const MyApp(),
     ),
   );
